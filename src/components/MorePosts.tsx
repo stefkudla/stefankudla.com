03ed8bc3190990d4071983345cdb7f85ffef179e
stefkudla <stefankudla@gmail.com>
2022-04-29 16:54:34 -0700

    temporary fix on morePosts

--- src/components/MorePosts.tsx
+++ src/components/MorePosts.tsx
@@ -1,17 +1,27 @@
 import PostPreview from './PostPreview'
 
-const MoreStories: React.FC = ({ posts }: { posts: any }) => {
+interface PostProps {
+  slug: string
+  title: string
+  metadata: {
+    excerpt: string
+  }
+  created_at: string
+  date: string
+}
+
+const MorePosts: React.FC = ({ posts }: any) => {
+  console.log(posts)
   return (
     <section>
       <h2 className="mb-8 text-xl font-bold tracking-tighter leading-tight">
         More Stories
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
-        {posts.map(post => (
+        {posts.map((post: PostProps) => (
           <PostPreview
             key={post.slug}
             title={post.title}
-            coverImage={post.metadata.cover_image}
             date={post.created_at}
             slug={post.slug}
             excerpt={post.metadata.excerpt}
@@ -21,4 +31,4 @@ const MoreStories: React.FC = ({ posts }: { posts: any }) => {
     </section>
   )
 }
-export default MoreStories
+export default MorePosts
