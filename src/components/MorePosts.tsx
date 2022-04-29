f14973db9770ea2fa16198ba76031fc5059cda0f
stefkudla <stefankudla@gmail.com>
2022-04-29 16:57:15 -0700

    commented code

--- src/components/MorePosts.tsx
+++ src/components/MorePosts.tsx
@@ -19,13 +19,13 @@ const MorePosts: React.FC = ({ posts }: any) => {
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post: PostProps) => (
-          <PostPreview
-            key={post.slug}
-            title={post.title}
-            date={post.created_at}
-            slug={post.slug}
-            excerpt={post.metadata.excerpt}
-          />
+          // <PostPreview
+          //   key={post.slug}
+          //   title={post.title}
+          //   slug={post.slug}
+          //   excerpt={post.metadata.excerpt}
+          // />
+          <></>
         ))}
       </div>
     </section>
