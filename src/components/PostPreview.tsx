de8e4cbb976c67c009ba74aef47176119445d4ce
stefkudla <stefankudla@gmail.com>
2022-04-29 17:03:13 -0700

    comment code so i can deploy

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -3,23 +3,20 @@ import Link from 'next/link'
 
 interface PreviewProps {
   title: string
-  coverImage: {
-    imgix_url: string
-  }
+  // coverImage: {
+  //   imgix_url: string
+  // }
+  imgix_url?: string
   excerpt: string
   slug: string
+  date: string
 }
 
-const PostPreview: React.FC<PreviewProps> = ({
-  title,
-  coverImage,
-  excerpt,
-  slug,
-}) => {
+const PostPreview: React.FC<PreviewProps> = ({ title, excerpt, slug }) => {
   return (
     <div>
       <div className="mb-5">
-        <CoverImage title={title} url={coverImage.imgix_url} />
+        {/* <CoverImage title={title} url={coverImage.imgix_url} /> */}
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link href={`/posts/${slug}`}>
