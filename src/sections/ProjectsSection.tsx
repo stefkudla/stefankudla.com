c5b89c6302dbf6cafd55e3830f210f971bd555b1
stefkudla <stefankudla@gmail.com>
2022-04-29 17:05:57 -0700

    remove temp data and implement actual data for postlist

--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -4,7 +4,7 @@ import { FlaskIcon } from '@/components/icons'
 import Link from 'next/link'
 
 interface ProjectsSectionProps {
-  posts: []
+  posts: any
 }
 
 const ProjectsSection: React.FC<ProjectsSectionProps> = ({ posts }) => {
@@ -14,63 +14,9 @@ const ProjectsSection: React.FC<ProjectsSectionProps> = ({ posts }) => {
         <div className="bg-back-subtle p-2 mr-4 rounded-full">
           <FlaskIcon />
         </div>
-        <h2 className="text-xl text-accent font-semibold">Projects</h2>
+        <h2 className="text-xl text-accent font-semibold">Works</h2>
       </span>
-      <ul>
-        <li className="py-5">
-          <Link href={'/'}>
-            <a className="group flex items-center justify-between px-8 py-5 -my-5 -mx-7 hover:bg-back-subtle transition-colors border-b-2">
-              <div className="max-w-lg">
-                <h3 className="text-xl font-bold mb-1 group-hover:text-accent transition-colors">
-                  Example
-                </h3>
-                <p className="text-sm text-fore-subtle">[...]</p>
-              </div>
-              <p>2022</p>
-            </a>
-          </Link>
-        </li>
-        <li className="py-5">
-          <Link href={'/'}>
-            <a className="group flex items-center justify-between px-8 py-5 -my-5 -mx-7 hover:bg-back-subtle transition-colors border-b-2">
-              <div className="max-w-lg">
-                <h3 className="text-xl font-bold mb-1 group-hover:text-accent transition-colors">
-                  Example
-                </h3>
-                <p className="text-sm text-fore-subtle">[...]</p>
-              </div>
-              <p>2022</p>
-            </a>
-          </Link>
-        </li>
-        <li className="py-5">
-          <Link href={'/'}>
-            <a className="group flex items-center justify-between px-8 py-5 -my-5 -mx-7 hover:bg-back-subtle transition-colors border-b-2">
-              <div className="max-w-lg">
-                <h3 className="text-xl font-bold mb-1 group-hover:text-accent transition-colors">
-                  Example
-                </h3>
-                <p className="text-sm text-fore-subtle">[...]</p>
-              </div>
-              <p>2022</p>
-            </a>
-          </Link>
-        </li>
-        <li className="py-5">
-          <Link href={'/'}>
-            <a className="group flex items-center justify-between px-8 py-5 -my-5 -mx-7 hover:bg-back-subtle transition-colors border-b-2">
-              <div className="max-w-lg">
-                <h3 className="text-xl font-bold mb-1 group-hover:text-accent transition-colors">
-                  Example
-                </h3>
-                <p className="text-sm text-fore-subtle">[...]</p>
-              </div>
-              <p>2022</p>
-            </a>
-          </Link>
-          {posts}
-        </li>
-      </ul>
+      <PostList allPosts={posts} bucketType="works" home={true} />
     </section>
   )
 }
