68c71a56802f940ea46278f4d9682290c60b2a95
stefkudla <stefankudla@gmail.com>
2022-05-02 16:09:26 -0700

    hide mesh on mobile devices

--- src/components/MeshBackground.tsx
+++ src/components/MeshBackground.tsx
@@ -14,15 +14,16 @@ const Circle: React.VFC<CircleProps> = ({ color }) => {
 }
 
 const MeshBackground: React.VFC = () => {
-  //   const colors = ['#2DDAFC', '#FCB72D', '#F52DFC', '#FC2D68']
   const colors = ['#17bce6', '#fee440', '#8338ec', '#e50051']
 
   return (
-    <div className="absolute sm:pl-48 sm:pt-48 -rotate-[45deg] grid grid-cols-2 gap-16 blur-[8rem] opacity-70 dark:opacity-30 scale-50 sm:scale-[.6] -z-30 pointer-events-none">
-      {colors.map(color => {
-        return <Circle color={color} key={color} />
-      })}
-    </div>
+    <>
+      <div className="hidden absolute pl-48 pt-48 -rotate-[45deg] md:grid grid-cols-2 gap-16 blur-[8rem] opacity-70 dark:opacity-30 scale-0 sm:scale-[.6] -z-30 pointer-events-none">
+        {colors.map(color => {
+          return <Circle color={color} key={color} />
+        })}
+      </div>
+    </>
   )
 }
 
