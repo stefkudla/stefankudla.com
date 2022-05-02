e2fb3b5fee4588bd74a95695e33eaebbf39fcda8
stefkudla <stefankudla@gmail.com>
2022-05-02 15:05:37 -0700

    remove margin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,14 +9,14 @@ const Header: React.VFC = () => {
     <>
       <header className="sm:pt-4 container max-w-screen-lg m-auto sm:px-12 md:px-20">
         <nav
-          className="hidden sm:flex items-center justify-start h-full mt-auto space-x-6 text-sm md:justify-start backdrop-filter backdrop-blur-sm  bg-opacity-30"
+          className="hidden sm:flex justify-start items-center h-full mt-auto space-x-6 text-sm md:justify-start backdrop-filter backdrop-blur-sm  bg-opacity-30"
           aria-label="Main Navigation"
         >
           <Logo />
           <MenuItems />
           <ThemeChanger
             styles={
-              'hidden mt-1 transition-transform ease-in-out focus:outline-none sm:block hover:text-accent group focus-visible:outline-accent'
+              'hidden transition-transform ease-in-out focus:outline-none sm:block hover:text-accent group focus-visible:outline-accent'
             }
           />
         </nav>
