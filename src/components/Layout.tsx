256c1f03c00604d089087be7ab4e157fadf0de33
stefkudla <stefankudla@gmail.com>
2022-05-03 16:58:34 -0700

    add page transitions with framer motion

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,26 +1,45 @@
 import React from 'react'
-import Alert from './AlertPreview'
 import Footer from './Footer'
 import Meta from './Meta'
 import Header from './Header'
+import { motion, AnimatePresence } from 'framer-motion'
 import 'lazysizes'
 import 'lazysizes/plugins/parent-fit/ls.parent-fit'
 
 interface LayoutProps {
   children: React.ReactNode
-  preview?: any
+  router: {
+    route: string
+  }
 }
 
-const Layout = ({ preview, children }: LayoutProps) => {
+const Layout = ({ children, router }: LayoutProps) => {
+  const variants = {
+    hidden: { opacity: 0, x: 0, y: 10 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: -0, y: 0 },
+  }
+
   return (
     <>
       <Meta />
       <Header />
-      <div className="flex flex-col min-h-screen">
-        <div className="container flex-grow max-w-screen-lg px-5 m-auto mt-16 sm:px-12 md:px-20">
-          {children}
-        </div>
-      </div>
+      <AnimatePresence exitBeforeEnter initial={true}>
+        <motion.div
+          key={router.route}
+          variants={variants}
+          initial="hidden"
+          animate="enter"
+          exit="exit"
+          transition={{ duration: 0.4, type: 'easeInOut' }}
+        >
+          <div className="flex flex-col min-h-screen">
+            <div className="container flex-grow max-w-screen-lg px-5 m-auto mt-16 sm:px-12 md:px-20">
+              {children}
+            </div>
+          </div>
+        </motion.div>
+      </AnimatePresence>
       <Footer />
     </>
   )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,10 @@ import Layout from '../components/Layout'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps, router }) => {
   return (
     <ThemeProvider attribute="class">
-      <Layout>
+      <Layout router={router}>
         <Component {...pageProps} />
       </Layout>
     </ThemeProvider>
