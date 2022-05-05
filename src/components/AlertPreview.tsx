faab24d49ea326d0becaad5db23bb23ccb271ae4
stefkudla <stefankudla@gmail.com>
2022-05-05 13:33:09 -0700

    change a tag to Link

--- src/components/AlertPreview.tsx
+++ src/components/AlertPreview.tsx
@@ -1,15 +1,14 @@
+import Link from 'next/link'
+
 const AlertPreview: any = ({ preview }: any) => {
   return preview ? (
     <div className="fixed text-fore-subtle left-5">
       <div className="py-2 text-center text-sm">
         <>
           This page is a draft.{' '}
-          <a
-            href="/api/exit-preview"
-            className="underline hover:text-cyan duration-200 transition-colors"
-          >
-            Click here
-          </a>{' '}
+          <Link href="/api/exit-preview">
+            <span className="underline hover:text-cyan duration-200 transition-colors"></span>
+          </Link>{' '}
           to exit preview mode.
         </>
       </div>
