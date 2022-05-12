a507ae241e6c0bddda6499d954a257d3f3582cfe
stefkudla <stefankudla@gmail.com>
2022-05-12 14:54:16 -0700

    add flex container for image

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,7 +4,7 @@ import { PaperIcon } from '@/components/icons'
 import SocialIcons from '@/components/SocialIcons'
 import SetupSection from '@/sections/SetupSection'
 import BrewSection from '@/sections/BrewSection'
-import { getAllProducts } from '@/lib/api'
+import { getAllProducts } from '@/lib/cosmic'
 import { ProductProps } from '@/types/product'
 
 const About: NextPage<ProductProps> = ({ allProducts }) => {
@@ -13,12 +13,14 @@ const About: NextPage<ProductProps> = ({ allProducts }) => {
       <section>
         <h1 className="text-4xl mb-12">About Me</h1>
         <div className="flex flex-col md:flex-row-reverse border-b pb-12">
-          <img
-            src="/images/stefan_kudla_spain_coast_square.png"
-            alt="Stefan Kudla on the coast of Spain"
-            className="rounded-md w-3/4 md:w-1/2 h-full"
-          />
-          <div className="mt-12 md:mt-0 flex flex-col gap-y-8 pr-20">
+          <div className="flex-1">
+            <img
+              src="/images/stefan_kudla_spain_coast_square.png"
+              alt="Stefan Kudla on the coast of Spain"
+              className="rounded-md w-full"
+            />
+          </div>
+          <div className="flex-1 mt-12 md:mt-0 flex flex-col gap-y-8 pr-20">
             <p>I love solving problems!</p>
             <p>
               My name is Stefan Kudla. I&apos;m the son of a Czechoslovakian man
