4961d8c6c1df2b44ce1cfc60cea86299219b2819
stefkudla <stefankudla@gmail.com>
2022-05-11 17:18:10 -0700

    create about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { NextPage } from 'next'
+import { PaperIcon } from '@/components/icons'
+import SocialIcons from '@/components/SocialIcons'
+import SetupSection from '@/sections/SetupSection'
+import BrewSection from '@/sections/BrewSection'
+import { getAllProducts } from '@/lib/api'
+import { ProductProps } from '@/types/product'
+import TopTracksSection from '@/sections/TopTracksSection'
+
+const About: NextPage<ProductProps> = ({ allProducts }) => {
+  return (
+    <>
+      <section>
+        <h1 className="text-4xl mb-12">About Me</h1>
+        <div className="flex flex-col md:flex-row-reverse border-b pb-12">
+          <img
+            src="/images/stefan_kudla_spain_coast_square.png"
+            alt="Stefan Kudla on the coast of Spain"
+            className="rounded-md w-3/4 md:w-1/2 h-full"
+          />
+          <div className="mt-12 md:mt-0 flex flex-col gap-y-8 pr-20">
+            <p>I love solving problems!</p>
+            <p>
+              My name is Stefan Kudla. I'm the son of a Czechoslovakian man who
+              defected from a soviet state and met a Seattle born woman here in
+              the US. I'm a constant student of life who's passionate about
+              bettering my skillset every single day!
+            </p>
+            <p>
+              I dedicate this site to my parents. I owe it to them for showing
+              me a beautiful world in which the possibilites are endless.
+            </p>
+            <div className="flex items-center md:mt-6">
+              <a
+                href="/Stefan_Kudla_Resume.pdf"
+                className="flex items-center mr-4 text-fore-primary border-2 border-accent w-fit px-4 py-1 rounded cursor-pointer hover:text-accent transition-colors"
+              >
+                <span className="mr-2">
+                  <PaperIcon />
+                </span>
+                Resume
+              </a>
+              <SocialIcons />
+            </div>
+          </div>
+        </div>
+      </section>
+      <SetupSection allProducts={allProducts} />
+      <BrewSection allProducts={allProducts} />
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const allProducts = (await getAllProducts()) || []
+  return {
+    props: { allProducts },
+  }
+}
+export default About
