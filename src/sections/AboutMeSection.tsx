62a91b5fa0d1f80c0523d75870aad414c6fbf7e0
stefkudla <stefankudla@gmail.com>
2022-05-14 11:11:51 -0700

    fix grammar in content

--- src/sections/AboutMeSection.tsx
+++ src/sections/AboutMeSection.tsx
@@ -12,14 +12,14 @@ const AboutMeSection: React.VFC = () => {
         I'm a software developer based in Las Vegas, Nevada.
       </p>
       <p className="text-fore-primary mb-8">
-        Coming from a background of audio engineering and music production, I
+        Coming from a background in audio engineering and music production, I
         quickly found that writing code was a bit like putting the secret sauce
         on a vocal mix; there's not necessarily a single way to do it, and the
-        person behind the keyboard has the power to put their own flavor into a
-        great solution.
+        person behind the keyboard has the power to put their style into a great
+        solution.
       </p>
       <p className="text-fore-primary mb-8">
-        My favourite part about coding is that there are so many different
+        My favorite part about coding is that there are so many different
         challenges to solve. I'm a problem solver at heart, and there seems to
         be an endless bank of tasks to hack away at.
       </p>
