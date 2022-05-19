682ef21a3fac5733343452026e6a38da85c4706d
stefkudla <stefankudla@gmail.com>
2022-05-18 19:00:44 -0700

    add aria labels to icons

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -4,15 +4,24 @@ import { EmailIcon, GithubIcon, LinkedinIcon } from '@/components/icons'
 const SocialIcons = () => {
   return (
     <span className="flex gap-x-5">
-      <a href="mailto:stefan@stefankudla.com" className="group cursor-pointer">
+      <a
+        href="mailto:stefan@stefankudla.com"
+        className="group cursor-pointer"
+        aria-label="Email"
+      >
         <EmailIcon styles="text-xl text-fore-subtle group-hover:-translate-y-2 transition-transform" />
       </a>
-      <a href="https://github.com/stefkudla" className="group cursor-pointer ">
+      <a
+        href="https://github.com/stefkudla"
+        className="group cursor-pointer"
+        aria-label="Github"
+      >
         <GithubIcon styles="text-xl text-fore-subtle group-hover:-translate-y-2 transition-transform" />
       </a>
       <a
         href="https://www.linkedin.com/in/stefankudla/"
         className="group cursor-pointer"
+        aria-label="Linkedin"
       >
         <LinkedinIcon styles="text-xl text-fore-subtle group-hover:-translate-y-2 transition-transform" />
       </a>
