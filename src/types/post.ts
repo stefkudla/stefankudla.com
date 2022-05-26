1a3885f0beb0414734016bff3b633577de1c1886
stefkudla <stefankudla@gmail.com>
2022-05-26 16:34:25 -0700

    add types for post header

--- src/types/post.ts
+++ src/types/post.ts
@@ -16,3 +16,35 @@ export interface PostTypes {
     created_at: string
   }
 }
+
+export interface PostHeaderTypes {
+  post: {
+    title: string
+    metadata: {
+      cover_image: {
+        imgix_url: string
+      }
+      published_date?: string
+      live_url?: string
+      repo_url?: string
+      category: string
+    }
+    created_at: string
+  }
+}
+
+export interface PostListTypes {
+  allPosts: [
+    {
+      title: string
+      slug: string
+      created_at: string
+      metadata: {
+        excerpt: string
+        published_date: string
+      }
+    }
+  ]
+  bucketType: string
+  home?: boolean
+}
