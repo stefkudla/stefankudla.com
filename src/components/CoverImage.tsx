32711abca5465cef21786213d4662c13876a4a98
stefkudla <stefankudla@gmail.com>
2022-06-11 11:10:52 -0700

    improve next image configuration

--- src/components/CoverImage.tsx
+++ src/components/CoverImage.tsx
@@ -3,17 +3,19 @@ import Image from 'next/image'
 const CoverImage: React.FC<{ title: string; url: string }> = ({
   title,
   url,
-}) => (
-  <div className="relative">
-    <Image
-      src={url}
-      width="100%"
-      height="75%"
-      alt={`Cover image for ${title}`}
-      layout="responsive"
-      objectFit="contain"
-      priority
-    />
-  </div>
-)
+}) => {
+  return (
+    <div className="relative w-full pb-[80%]">
+      <Image
+        src={url}
+        quality={60}
+        alt={`Cover image for ${title}`}
+        layout="fill"
+        objectFit="contain"
+        placeholder="blur"
+        blurDataURL={`${url}?auto=format,compress&q=1&blur=500&w=300`}
+      />
+    </div>
+  )
+}
 export default CoverImage
