4e1e51d544bd9832946db95b84c2e1833e444765
stefkudla <stefankudla@gmail.com>
2022-06-21 10:41:02 -0700

    update config

--- next-sitemap-config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  siteUrl: 'https://stefankudla.com',
-  generateRobotsTxt: true,
-}
---
+++ next-sitemap.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+
+const config = {
+  siteUrl: 'https://stefankudla.com',
+  generateIndexSitemap: false,
+  generateRobotsTxt: true,
+}
+export default config
