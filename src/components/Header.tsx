2703755dd409c2ba0c135e83a27327420d8410fb
stefkudla <stefankudla@gmail.com>
2022-07-20 15:46:21 -0400

    update styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import CurrentlyPlaying from './CurrentlyPlaying'
 const Header: React.FC = () => (
   <header className="md:pt-4 container max-w-screen-lg m-auto md:px-12 lg:px-20">
     <nav
-      className="hidden md:flex justify-start items-center h-full mt-auto space-x-6 text-sm lg:justify-start backdrop-filter backdrop-blur-sm  bg-opacity-30"
+      className="hidden md:flex justify-start items-center h-full w-full mt-auto space-x-6 text-sm lg:justify-start backdrop-filter backdrop-blur-sm  bg-opacity-30"
       aria-label="Main Navigation"
     >
       <Logo />
