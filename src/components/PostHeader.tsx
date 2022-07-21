3013b1c538afd427a0742e913d84f56e3103788d
stefkudla <stefankudla@gmail.com>
2022-07-21 15:30:54 -0400

    remove priority tag from avatar

--- src/components/PostHeader.tsx
+++ src/components/PostHeader.tsx
@@ -20,7 +20,6 @@ const PostHeader: React.FC<PostHeaderTypes> = ({ post }) => {
               height={80}
               alt="Stefan Kudla"
               className="rounded-full"
-              priority
             />
           </div>
           <p className="ml-2 text-sm">
