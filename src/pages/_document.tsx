9d06abeec6e0b5865d92dd44841ec2a6beafd2d4
stefkudla <stefankudla@gmail.com>
2022-08-11 15:24:28 -0400

    convert gtm scripts to Nextjs Script component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 const Document = () => {
   return (
     <Html lang="en">
       <Head>
-        <script
-          async
+        <Script
+          strategy="lazyOnload"
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
-        <script
+        <Script
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
             window.dataLayer = window.dataLayer || [];
