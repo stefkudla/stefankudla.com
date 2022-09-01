366bfd5db641f1f9975988c21bb4b0ae6e368686
stefkudla <stefankudla@gmail.com>
2022-09-01 08:55:11 -0400

    remove google analytics tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,29 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 const Document = () => {
   return (
     <Html lang="en">
-      <Head>
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <Script
-          strategy="afterInteractive"
-          id="google-tag-manager"
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
