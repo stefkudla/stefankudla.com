5aebc7445e108e15ad01881ada94671ade480200
stefkudla <stefankudla@gmail.com>
2023-04-20 16:02:26 -0400

    add Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
@@ -10,6 +11,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
       disableTransitionOnChange
     >
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   )
 }
