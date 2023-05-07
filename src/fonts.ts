0ffe5999ac64bd5c01a38fa997e209543c6312de
stefkudla <stefankudla@gmail.com>
2023-05-07 18:30:09 -0400

    add medium font weight (Oswald)

--- src/fonts.ts
+++ src/fonts.ts
@@ -10,7 +10,7 @@ export const nunitoSans = Nunito_Sans({
 
 export const oswald = Oswald({
   display: 'swap',
-  weight: ['400', '600', '700'],
+  weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
   variable: '--font-oswald',
   fallback: ['system-ui', 'arial'],
